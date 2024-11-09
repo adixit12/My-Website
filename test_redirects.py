ce882d5ea5bb3f3038f14b9b11a87238d2e5046c
adixit12 <45751225+adixit12@users.noreply.github.com>
2024-11-09 19:03:33 +1100

    Update test_redirects.py

--- test_redirects.py
+++ test_redirects.py
@@ -1,22 +1,17 @@
 import requests
 import json
 import os
-from akamai.edgegrid import EdgeGridAuth, EdgeRc
+from akamai.edgegrid import EdgeGridAuth
 #from urllib.parse import urljoin
 
-edgerc = EdgeRc('.edgerc')
-section = 'default'
-baseurl = 'https://akab-xe3dt34aoi3hcmul-y3mjulo6d64sk6tb.luna.akamaiapis.net'
-
-# s = requests.Session()
-# s.auth = EdgeGridAuth(
-#     client_token=os.environ['client_token'],
-#     client_secret=os.environ['client_secret'],
-#     access_token=os.environ['access_token']
-# )
+baseurl = 'https://akab-xe3dt34aoi3hcmul-y3mjulo6d64sk6tb.luna.akamaiapis.net'  ## Can be used to identify which API client is used.
 
 s = requests.Session()
-s.auth = EdgeGridAuth.from_edgerc(edgerc, section)
+s.auth = EdgeGridAuth(
+    client_token=os.environ['client_token'],
+    client_secret=os.environ['client_secret'],
+    access_token=os.environ['access_token']
+) ## fetched from environment variables defined in the .github/workflows yml file. 
 
 # ---------------------------
 
